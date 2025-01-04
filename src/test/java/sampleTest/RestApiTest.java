e42c4159e1ceea691250768dba864177a4f1dbce
Ajay Kumar <ajayukumaru1999@gmail.com>
2025-01-04 17:29:34 +0530

    intellij

--- src/test/java/sampleTest/RestApiTest.java
+++ src/test/java/sampleTest/RestApiTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
-public class RestApi {
+public class RestApiTest {
 
     @Test
     public void testRestAPI(){
